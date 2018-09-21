c21632f1ed1f94b3e0782030aa55484935b19082
Guilherme Varandas <jgvarandas@gmail.com>
2018-09-21 18:41:35 -0300

    🌎 Fetching API data

--- App.js
+++ App.js
@@ -1,33 +1,49 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 
 import Card from './components/Card';
 
+const API_HOST = 'https://rickandmortyapi.com/api/';
+
 export default class App extends React.Component {
+  
+  state = {
+    characters: [],
+  }
+
+  componentDidMount() {
+    this._getCharacters();
+  }
+
+  async _getCharacters() {
+    const response = await fetch(`${API_HOST}/character`);
+    const { results: characters } = await response.json();
+    this.setState({
+      characters,
+    });
+  }
+
+  renderCharacter = ({ item: character }) => {
+    const { name, status, gender } = character;
+    return (
+      <Card>
+        <Text>Name: {name}</Text>
+        <Text>Gender: {gender}</Text>
+        <Text>Status: {status}</Text>
+      </Card>
+    );
+  }
+
+  _keyExtractor = item => item.id.toString();
+
   render() {
     return (
       <View style={styles.container}>
-        <Card>
-          <Text>
-            Sed a ipsum et nisi ultricies pulvinar.
-            Nulla lobortis, nibh id convallis fringilla, metus tortor aliquet mauris, feugiat vehicula nisl mauris et lectus. In vitae felis mauris.
-            Aenean fermentum blandit erat et maximus. In magna tortor, tincidunt id elit vel, condimentum ornare massa. Maecenas sed arcu est
-          </Text>
-        </Card>
-        <Card>
-          <Text>
-            Suspendisse aliquet est id congue lacinia.
-            Aliquam elementum ligula ligula, quis suscipit lectus semper id.
-            Pellentesque posuere justo eu urna viverra pellentesque. Vestibulum euismod dolor vel ante euismod aliquam.
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tincidunt cursus elementum.
-          </Text>
-        </Card>
-        <Card>
-          <Text>
-            Duis convallis euismod facilisis. Nam vehicula lectus ac ligula varius, et tincidunt ante dapibus.
-            Sed rutrum sed ligula et lobortis. Cras eu venenatis nibh. Fusce at arcu at urna eleifend aliquam mollis mollis erat.
-          </Text>
-        </Card>
+        <FlatList
+          data={this.state.characters}
+          renderItem={this.renderCharacter}
+          keyExtractor={this._keyExtractor}
+        />
       </View>
     );
   }
@@ -35,9 +51,9 @@ export default class App extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
+    paddingTop: 30,
     flex: 1,
     backgroundColor: '#d9d9d9',
-    alignItems: 'center',
     justifyContent: 'center',
   },
 });
